e913d89d493e522733fcf2be40a957112f59e263
TianYe <tianye4@lenovo.com>
2015-05-12 18:44:04 +0800

    background

--- URDRS/app/src/main/java/com/lenovo/health/urdrs/Fragment1.java
+++ URDRS/app/src/main/java/com/lenovo/health/urdrs/Fragment1.java
@@ -1,12 +1,27 @@
 package com.lenovo.health.urdrs;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Iterator;
+import java.util.List;
 
 
 /**
@@ -26,9 +41,32 @@ public class Fragment1 extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private TextView locText;
     private OnFragmentInteractionListener mListener;
+    private LocationManager locationManager;
+    private LocationListener locListner = new LocationListener() {
 
+        @Override
+        public void onLocationChanged(Location location) {
+            System.out.println("Location Change");
+            updateWithNewLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            getLocation();
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            updateWithNewLocation(null);
+        }
+    };
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
@@ -58,14 +96,60 @@ public class Fragment1 extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
+    public void getLocation() {
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        String provider = locationManager.getBestProvider(criteria, true);
+        System.out.println(provider);
+        Location lastKnownLocation = locationManager.getLastKnownLocation(provider);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 1, locListner);
+        updateWithNewLocation(lastKnownLocation);
+    }
+
+    public void printLastLocation() {
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if (lastLocation != null) {
+            locText.setText(lastLocation.toString());
+        }
+        List<String> providers = locationManager.getAllProviders();
+        for(Iterator iterator = providers.iterator(); iterator.hasNext(); ){
+            System.out.println(iterator.next());
+        }
+    }
+
+    public void updateWithNewLocation(Location location){
+        String loc;
+        if (location != null) {
+            double lat = location.getLatitude();
+            double lon = location.getLongitude();
+            loc = "经度" + lat + "纬度" + lon;
+            locText.setText(loc);
+        }
+        else {
+            locText.setText("不能获取位置");
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment1, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment1, container, false);
+        locText = (TextView) view.findViewById(R.id.location_textview);
+        TextView testview = (TextView) view.findViewById(R.id.question1);
+        testview.setText("WWWW");
+        getLocation();
+        return view;
     }
+
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -90,6 +174,7 @@ public class Fragment1 extends Fragment {
         super.onDetach();
         mListener = null;
     }
+
 
     /**
      * This interface must be implemented by activities that contain this
